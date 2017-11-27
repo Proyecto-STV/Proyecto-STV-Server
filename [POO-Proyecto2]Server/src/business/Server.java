b186a2eb09788b1b4cec45dcf21cf36716996ae0
Juan Pablo Soto Rojas <23349398+Juan-Pablo-Soto@users.noreply.github.com>
2017-11-27 10:17:19 -0600

    Update Server.java

--- [POO-Proyecto2]Server/src/business/Server.java
+++ [POO-Proyecto2]Server/src/business/Server.java
@@ -180,7 +180,7 @@ public class Server extends Thread implements IConstants {
     }
 
     public int contar_votos() {        
-        int necesario = (votos.size() / puesto.getNumero() + 1) + 1; //Es drop
+        int necesario = (listaVotos.size() / puesto.getNumero() + 1) + 1; //Es drop
         System.out.println("La cantidad de votos es: ");
         System.out.println(listaVotos.size());
         System.out.println("Los votos necesarios para ganar es");
